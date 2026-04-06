0e412a51800b5c6a58adff7ede4d96e93439c14e
xiaoheng hu <huxiaoheng@tum.de>
2026-04-06 17:11:21 +0000

    fix frontend build warnings and pdf setup

--- react-front/src/components/Footer.js
+++ react-front/src/components/Footer.js
@@ -2,13 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer >
+    <footer>
       <div className="container mx-auto text-center flex justify-center items-center space-x-4">
         <p className="text-lg font-bold">Xiaoheng Hu</p>
         <p>Phone: +49 1623384004</p>
-        <p>
-            // social media icons
-        </p>
         <p className="mt-2">&copy; 2024 Xiaoheng Hu. All rights reserved.</p>
       </div>
     </footer>
@@ -16,4 +13,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
